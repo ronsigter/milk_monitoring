1d30c627ac353f9028cdca88569409bfe0e37fd2
ronsigter <email@example.com>
2020-04-10 22:48:33 +0800

    Updates

---
+++ precache-manifest.4a1da1596e062aa78ea5f1ecb404ea0c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "051cda5e099d8066311283a3fec48d11",
+    "url": "/milk_monitoring/index.html"
+  },
+  {
+    "revision": "084366b955b7af9603bc",
+    "url": "/milk_monitoring/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "084366b955b7af9603bc",
+    "url": "/milk_monitoring/static/js/2.f2f6ac7c.chunk.js"
+  },
+  {
+    "revision": "e899412d8dbefb133da0034f43167f5d",
+    "url": "/milk_monitoring/static/js/2.f2f6ac7c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8caa7bdc6ed5c340b302",
+    "url": "/milk_monitoring/static/js/main.0e7ce177.chunk.js"
+  },
+  {
+    "revision": "233231701f7f13e03321",
+    "url": "/milk_monitoring/static/js/runtime-main.bf47cf16.js"
+  }
+]);
